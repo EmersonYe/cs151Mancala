ac2affab61e646320d74d174bbcab38322cd0e0e
Karl Adrian Lapuz <karl.adrian25@yahoo.com>
2017-05-02 16:02:41 -0700

    Updated Model with more methods

--- src/DataModel.java
+++ src/DataModel.java
@@ -10,6 +10,7 @@ import javax.swing.event.ChangeListener;
 public class DataModel {
 	
 	private boolean isPlayerAsTurn;
+	private int numStones;
 	private int[] pits;
 	private int[] clone;
 	private int undos;
@@ -25,14 +26,14 @@ public class DataModel {
 	 * Create DataModel object with all pits filled with given number of stones
 	 * @param initialStoneCount number of stones to put in each pit
 	 */
-	public DataModel(int initialStoneCount)
+	public DataModel()
 	{
 		pits = new int[14];
 		for (int i = 1; i <= 13; i++) // 0th and 13th indeces are mancalas
 		{
 			if (i != PLAYER_A_MANCALA)
 			{
-				pits[i] = initialStoneCount;
+				pits[i] = numStones;
 			}
 		}
 		clone = pits.clone();
@@ -42,14 +43,68 @@ public class DataModel {
 		undos = 0;
 	}
 	
+	public String getTurn()
+	{
+		if (isPlayerAsTurn)
+		{
+			return "Player A";
+		}
+		return "Player B";
+	}
+	
+	public void setTurn()
+	{
+		if (lastStonePlaced == PLAYER_A_MANCALA && isPlayerAsTurn)
+		{
+			isPlayerAsTurn = true;
+		}
+		else if (lastStonePlaced == PLAYER_B_MANCALA && !isPlayerAsTurn)
+		{
+			isPlayerAsTurn = false;
+		}
+		else
+		{
+			isPlayerAsTurn = !isPlayerAsTurn;
+		}
+	}
+	
+	public boolean isMancala(int pit)
+	{
+		return (pit == PLAYER_A_MANCALA || pit == PLAYER_B_MANCALA);
+	}
+	
+	public boolean isOwnPit(int pit)
+	{
+		if (isPlayerAsTurn && pit >= 1 && pit < 7)
+		{
+			return true;
+		}
+		else if (!isPlayerAsTurn && pit >= 8 && pit < 13)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	public int getLastStonePlacement()
+	{
+		return lastStonePlaced;
+	}
+	
 	public String toString()
 	{
-		String pitData = "[" + pits[0];
-		for (int i = 1; i <= 13; i++)
+		String pitData = "";
+		pitData += pits[0];
+		for (int i = 13; i >= 8; i--)
 		{
 			pitData += ", " + pits[i];
 		}
-		return pitData + "]";
+		pitData += "\n   " + pits[1];
+		for (int i = 2; i <= 7; i++)
+		{
+			pitData += ", " + pits[i];
+		}
+		return pitData;
 	}
 	
 	public int[] getBoardData()
@@ -81,31 +136,59 @@ public class DataModel {
 		{
 			if (isPlayerAsTurn)
 			{
-				return false;
+				return true;
 			}
-			return true;
+			System.out.println("Can't access that pit");
+			return false;
 		}
 		else if (pitChosen >= 8 && pitChosen <= 13)
 		{
 			if (isPlayerAsTurn)
 			{
-				return true;
+				System.out.println("Can't access that pit");
+				return false;
 			}
-			return false;
+			return true;
 		}
+		System.out.println("That's a mancala");
+		return false;
 	}
 	
-	public void move(int pitChosen)
+	public void move(int pit)
 	{
-		if (isMoveValid(pitChosen))
+		if (isMoveValid(pit))
 		{
 			clone = pits.clone();
-			int numStones = pits[pitChosen];
-			pits[pitChosen] = 0;
+			int numStones = pits[pit];
+			pits[pit] = 0;
 			
 			for (int i = 1; i <= numStones; i++)
 			{
-				if ()
+				pit++;
+				if (pit == PLAYER_A_MANCALA && !isPlayerAsTurn || pit == PLAYER_B_MANCALA
+						&& isPlayerAsTurn)
+				{
+					i--; // don't lose a stone
+				}
+				else
+				{
+					pits[pit % 14]++;
+				}
+			}
+			lastStonePlaced = pit % 14;
+			if (pits[lastStonePlaced] == 1 && !isMancala(lastStonePlaced) 
+					&& isOwnPit(lastStonePlaced))
+			{
+				captureOpposite(lastStonePlaced);
+			}
+			
+			if (isGameOver())
+			{
+				captureAllStones();
+			}
+			else
+			{
+				setTurn();
 			}
 		}
 	}
@@ -133,5 +216,33 @@ public class DataModel {
 		}
 		pits[PLAYER_A_MANCALA] += playerA;
 		pits[PLAYER_B_MANCALA] += playerB;
+	}
+	
+	public boolean isGameOver()
+	{
+		int playerAStones = 0;
+		int playerBStones = 0;
+		for (int i = 1; i <= 6; i++)
+		{
+			playerAStones += pits[i];
+			playerBStones += pits[i + 7];
+		}
+		return (playerAStones == 0 || playerBStones == 0);
+	}
+	
+	public String getWinner()
+	{
+		if (pits[PLAYER_A_MANCALA] > pits[PLAYER_B_MANCALA])
+		{
+			return "Player A wins!";
+		}
+		else if (pits[PLAYER_B_MANCALA] > pits[PLAYER_A_MANCALA])
+		{
+			return "Player B wins!";
+		}
+		else
+		{
+			return "It's a draw!";
+		}
 	}
 }
